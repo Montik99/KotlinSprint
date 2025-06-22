98282588328110637b27180aa16acdf5d5b1d5ad
Денис <montik.den@yandex.ru>
2025-06-22 16:45:40 +0300

    Instal Commit

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -5,16 +5,12 @@ import org.example.utils.Printer
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    val message = "Hello, " + name + "!"
-    val printer = Printer(message)
-    printer.printMessage()
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val totalNumberOfOrders: Int = 34 // общее количество заказов
+    val orderThankYouMessage: String = "Спасибо за ваш заказ!"
+
+
+    println(totalNumberOfOrders)
+    println(orderThankYouMessage)
 }
+
